35562ec25aaaa2c89ae37c61da54a945657a45b9
Joseph Nevado <josephnevado12@gmail.com>
2022-09-14 15:38:28 -0700

    Initial Commit

---
+++ SpotterApp/SpotterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotterAppApp.swift
+//  SpotterApp
+//
+//  Created by Joseph Nevado on 2022-09-14.
+//
+
+import SwiftUI
+
+@main
+struct SpotterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
