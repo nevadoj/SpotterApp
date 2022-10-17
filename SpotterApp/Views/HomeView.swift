5f985e25b0bb62897f06e290af608b680e062ee0
Joseph Nevado <josephnevado12@gmail.com>
2022-10-17 15:40:24 -0700

    move add button to corner

--- SpotterApp/Views/HomeView.swift
+++ SpotterApp/Views/HomeView.swift
@@ -18,69 +18,77 @@ struct HomeView: View {
     var body: some View{
         ZStack(alignment: .bottomTrailing){
             NavigationView{
-                ScrollView{
-                    VStack{
-                        ForEach(program) { program in
-                            DisclosureGroup{
-                                if program.size > 0{
-                                    ForEach(program.exercises!){ exercise in
-                                        ExerciseItemView(exercise: exercise)
-                                            .onTapGesture {
-                                                showExerciseDetails = true
-                                            }.sheet(isPresented: $showExerciseDetails){
-                                                NavigationView{
-                                                    ExerciseSheetView()
-                                                        .navigationTitle(exercise.name!)
+                ZStack{
+                    ScrollView{
+                        VStack{
+                            ForEach(program) { program in
+                                DisclosureGroup{
+                                    if program.size > 0{
+                                        ForEach(program.exercises!){ exercise in
+                                            ExerciseItemView(exercise: exercise)
+                                                .onTapGesture {
+                                                    showExerciseDetails = true
+                                                }.sheet(isPresented: $showExerciseDetails){
+                                                    NavigationView{
+                                                        ExerciseSheetView()
+                                                            .navigationTitle(exercise.name!)
+                                                    }
+                                                    .presentationDetents([.medium])
                                                 }
-                                                .presentationDetents([.medium])
-                                            }
-                                            .swipeActions(edge: .trailing, allowsFullSwipe: false){
-                                                Button(role: .destructive){
-                                                    Swift.print("Testing")
-                                                } label:{
-                                                    Label("Delete2", systemImage: "trash")
+                                                .swipeActions(edge: .trailing, allowsFullSwipe: false){
+                                                    Button(role: .destructive){
+                                                        Swift.print("Testing")
+                                                    } label:{
+                                                        Label("Delete2", systemImage: "trash")
+                                                    }
                                                 }
-                                            }
+                                        }
+                                    }
+                                } label: {
+                                    VStack(alignment: .leading){
+                                        Text(program.name!)
+                                            .font(.body)
+                                            .fontWeight(.bold)
+                                            .padding(.vertical)
+                                        HStack(alignment: .firstTextBaseline){
+                                            Text("Working Sets: 12")
+                                                .font(.footnote)
+                                                .foregroundColor(.secondary)
+                                            Text("Exercises: \(program.size)")
+                                                .font(.footnote)
+                                                .foregroundColor(.secondary)
+                                        }
+                                    }
+                                    // Need to find method or re-design a way to edit programs (if a user is wrong the first time, they delete? -- push editing back)
+                                    .swipeActions(edge: .trailing, allowsFullSwipe: false){
+                                        Button{
+                                            Swift.print("Deleting Program")
+                                        } label:{
+                                            Label("", systemImage: "trash")
+                                        }
+                                        .tint(.red)
+                                    }
+                                    .contextMenu{
+                                        Button(role: .destructive){
+                                            deleteProgram(program)
+                                        } label: {
+                                            Label("Delete Routine", systemImage: "trash")
+                                        }
                                     }
                                 }
-                            } label: {
-                                VStack(alignment: .leading){
-                                    Text(program.name!)
-                                        .font(.body)
-                                        .fontWeight(.bold)
-                                        .padding(.vertical)
-                                    HStack(alignment: .firstTextBaseline){
-                                        Text("Working Sets: 12")
-                                            .font(.footnote)
-                                            .foregroundColor(.secondary)
-                                        Text("Exercises: \(program.size)")
-                                            .font(.footnote)
-                                            .foregroundColor(.secondary)
-                                    }
-                                }
-                                // Need to find method or re-design a way to edit programs (if a user is wrong the first time, they delete? -- push editing back)
-                                .swipeActions(edge: .trailing, allowsFullSwipe: false){
-                                    Button{
-                                        Swift.print("Deleting Program")
-                                    } label:{
-                                        Label("", systemImage: "trash")
-                                    }
-                                    .tint(.red)
-                                }
-                                .contextMenu{
-                                    Button(role: .destructive){
-                                        deleteProgram(program)
-                                    } label: {
-                                        Label("Delete Routine", systemImage: "trash")
-                                    }
-                                }
+                                .padding()
+                                .background(RoundedRectangle(cornerRadius: 20)
+                                    .fill(Color.green)
+                                    .opacity(0.1)
+                                )
                             }
-                            .padding()
-                            .background(RoundedRectangle(cornerRadius: 20)
-                                .fill(Color.green)
-                                .opacity(0.1)
-                            )
                         }
+                        .navigationTitle("Programs")
+                        
+                    }
+                    
+                    VStack{
+                        Spacer()
                         HStack {
                             Spacer()
                             NavigationLink{
@@ -91,7 +99,6 @@ struct HomeView: View {
                             }
                         }
                     }
-                    .navigationTitle("Programs")
                 }
             }
         }
